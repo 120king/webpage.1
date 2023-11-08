c39805bd8cc5f894e255a4d00a40c3e3564e1f43
120king <145893291+120king@users.noreply.github.com>
2023-11-08 19:09:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+ /* document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll("nav ul li");
+
+    navLinks.forEach((link) => {
+      link.addEventListener("click", function () {
+        // Remove the 'active' class from all links
+        navLinks.forEach((el) => el.classList.remove("active"));
+        // Add the 'active' class to the clicked link
+        this.classList.add("active");
+      });
+    });
+  });*/
